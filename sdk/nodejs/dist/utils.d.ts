c42eccbbc9db04a7463f55d9fd52f8694a958d93
evlon <liamgnc@gmail.com>
2026-05-15 12:13:26 +0800

    删除 sdk

--- sdk/nodejs/dist/utils.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function sleep(ms: number): Promise<void>;
-export declare function randomInt(min: number, max: number): number;
-export declare function randomFloat(min: number, max: number): number;
-export declare function buildWindowSelector(selector: {
-    title?: string;
-    className?: string;
-    processName?: string;
-}): string;
-//# sourceMappingURL=utils.d.ts.map
